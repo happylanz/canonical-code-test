baa926e7b072b6918448fce886e20ab0984ea848
Land <milano_roland@yahoo.com>
2023-10-07 22:34:44 +0800

    (feat) add loader while fetching

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,37 @@ import "./App.styles.scss";
 
 function App() {
   const [posts, setPosts] = useState<Array<ICard>>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchPosts().then(setPosts);
+    fetchPosts()
+      .then(setPosts)
+      .then(() => setLoading(false));
   }, []);
 
   return (
     <main>
       <section id="main-section">
-        <div className="row">
-          {posts.map((post) => (
-            <div key={post.id} className="col-4 u-equal-height">
-              <Card {...post} />
-            </div>
-          ))}
-        </div>
+        {/* LOADER */}
+        {loading && (
+          <div className="u-align-text--center loader-container">
+            <span className="u-sh3">
+              FETCHING ARTICLES{" "}
+              <i className="p-icon--spinner u-animation--spin" />
+            </span>
+          </div>
+        )}
+
+        {/* POSTS */}
+        {!loading && (
+          <div className="row">
+            {posts.map((post) => (
+              <div key={post.id} className="col-4 u-equal-height">
+                <Card {...post} />
+              </div>
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
